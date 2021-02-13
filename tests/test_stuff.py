6b8a4b0ad62a28730206d001f22cb7498f402e7a
Hinrich Mahler <hinrich.mahler@freenet.de>
2021-02-13 17:49:50 +0100

    stuff

--- tests/test_stuff.py
+++ tests/test_stuff.py
@@ -1,5 +1,5 @@
 import os
 
-
-def test_env_var():
-    print(repr(os.getenv('TEST_NO_PASSPORT', 'NO SUCH ENVVAR')))
+class TestStuff:
+    def test_env_var():
+        print(repr(os.getenv('TEST_NO_PASSPORT', 'NO SUCH ENVVAR')))
